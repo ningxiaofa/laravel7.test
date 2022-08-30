8eab79f01d4bb566da6a9d19819f6e5501ed0ee1
william ning <1158885641@qq.com>
2022-08-30 16:11:41 +0800

    Test vhost -- normal

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class IndexController extends Controller
+{
+    public function index()
+    {
+        return [
+            'code' => 200,
+            'msg' => 'Success',
+            'data' => [
+                'name' => 'william ning',
+                'age' => 29
+            ]
+        ];
+    }
+
+    public function view()
+    {
+        return view('welcome');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/index', 'IndexController@index');
+
+Route::get('/index/view', 'IndexController@view');
